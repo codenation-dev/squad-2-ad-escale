45c4acee38d5faecbe5e97aaa9e8671064dc0e76
anevaraz <danielgutierrez.info@gmail.com>
2019-08-05 23:14:14 -0300

    Estilo global e Rotas criadas

---
+++ frontend/src/pages/SignIn/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { IoMdPaw } from 'react-icons/io';
+import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <>
+      <Container>
+        <h1>
+          <IoMdPaw />
+        </h1>
+      </Container>
+    </>
+  );
+}
---
+++ frontend/src/routes/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
+
+import Home from '../pages/Home';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+import Profile from '../pages/Profile';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/profile" component={Profile} isPrivate />
+    </Switch>
+  );
+}
