9be984110edc6c58888683845dcfa0edd964d4d7
Saulo Silva Castilhos <altfreedom@gmail.com>
2019-08-12 15:01:54 -0300

    new

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Home() {
   return <h1>Home</h1>;
---
+++ frontend/src/pages/Home/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 350px;
+  background: #0000cd;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  padding: 30px;
+  margin: 80px auto;
+`;
