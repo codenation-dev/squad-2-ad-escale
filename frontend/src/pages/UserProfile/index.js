0b1266317c1f076cfdd19f39d63d10abd34f7f6f
heliobreve <habreve@gmail.com>
2019-08-12 21:49:15 -0300

    criacao de componente UserProfile

---
+++ frontend/src/pages/UserProfile/index.js
@@ -0,0 +1,7 @@
+// pagina de perfil de usuario
+
+import React from 'react';
+
+export default function UserProfile() {
+  return <h1>Home</h1>;
+}
---
+++ frontend/src/pages/UserProfile/styles.js
@@ -0,0 +1,12 @@
+// pagina de estilos do perfil pet
+
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 350px;
+  background: #0000cd;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  padding: 30px;
+  margin: 80px auto;
+`;
