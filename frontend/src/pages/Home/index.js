efcb3ac4433684e89ab7acf72cfa710b17b4efad
Saulo Silva Castilhos <altfreedom@gmail.com>
2019-08-13 19:04:15 -0300

    first version home page

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,11 +1,52 @@
 // pagina home(inicial)
 
 import React from 'react';
+import data from '../../data';
 
-export default function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+export default class Home extends React.Component {
+  state = {
+    pets: [],
+    filteredPets: [],
+  };
+
+  componentWillMount() {
+    this.setState({
+      pets: [...data],
+      filteredPets: [...data],
+    });
+  }
+
+  handleButtons = e => {
+    let filteredPets = [...this.state.pets];
+    filteredPets = filteredPets.filter(item => {
+      return item.status === e.target.value;
+    });
+    this.setState({
+      filteredPets: filteredPets,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div className="row">
+          <button onClick={this.handleButtons} value="achados">
+            Achados
+          </button>
+          <button onClick={this.handleButtons} value="perdidos">
+            Perdidos
+          </button>
+          <button onClick={this.handleButtons} value="adocao">
+            Adoção
+          </button>
+
+          {this.state.filteredPets.map(item => {
+            return (
+              <h1>{`${item.especie}, ${item.nome}, ${item.raca}, ${item.status}.`}</h1>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
 }
