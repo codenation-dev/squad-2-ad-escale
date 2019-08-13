943e27e7740fec853d70366e38ef5f59b122552e
anevaraz <danielgutierrez.info@gmail.com>
2019-08-12 22:15:40 -0300

    Ajustes projeto rodando

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,9 +1,25 @@
 // pagina de cadastro
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { IoMdPaw } from 'react-icons/io'; // importando icone de uma biblioteca padrao
+import { IoMdPaw } from 'react-icons/io'; // importação do icone pet de uma biblioteca
 
 export default function SignUp() {
-  return <h1>SignUp</h1>;
+  return (
+    <>
+      <h1>
+        <IoMdPaw size="135" />
+        <h2>Pet Codes</h2>
+      </h1>
+      <form>
+        <input placeholder="Nome Completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua Senha" />
+
+        <button type="submit">Criar Conta</button>
+        <Link to="/signin">Já tenho uma conta</Link>
+      </form>
+    </>
+  );
 }
