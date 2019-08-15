506a358d50f8477b738a68c92a510a8de835fb76
heliobreve <habreve@gmail.com>
2019-08-15 14:07:39 -0300

    Manipulador de evento implementado em UserProfile

--- frontend/src/pages/UserProfile/index.js
+++ frontend/src/pages/UserProfile/index.js
@@ -2,35 +2,46 @@
 
 import React from 'react';
 
-export default function UserProfile() {
-  return (
-    <>
-      <h1>
-        <h2>Pet Codes</h2>
-      </h1>
-      <form>
-        Nome: <input type="text" placeholder="Seu nome" value="Helio" />
-        Sobrenome:{' '}
-        <input type="text" placeholder="Seu sobrenome" value="Breve" />
-        Data de Nascimento: <input type="date" value="" />
-        Telefone:{' '}
-        <input type="tel" placeholder="Seu telefone" value="11987654321" />
-        Endereço: <input type="text" placeholder="Seu endereco" value="" />
-        Número: <input type="number" placeholder="111" value="" />
-        Complemento:{' '}
-        <input type="text" placeholder="Apartamento, bloco, casa" value="12A" />
-        Bairro:
-        <input type="text" placeholder="Seu bairro" value="" />
-        Cidade:
-        <input type="text" placeholder="Nome Cidade" value="" />
-        Estado:
-        <input type="text" placeholder="AA" value="" />
-        CEP:
-        <input type="number" placeholder="00000-000" value="" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua Senha" />
-        <button type="submit">Acessar</button>
-      </form>
-    </>
-  );
+export default class UserProfile extends React.Component {
+  
+  state =  { 
+
+  };
+  handleChangeInput = e =>{
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  
+  componentWillMount(){
+    
+    
+    this.setState({
+      
+    })
+
+
+  }
+  
+  
+  render() {
+    return (
+      <>
+        <h1>
+          <h2>User Profile</h2>
+        </h1>
+        <form>
+          <input name="name" type="text" placeholder="Seu Nome" value={this.state.name} onChange = {(e) => {this.handleChangeInput(e)}} />
+          <input name="lastname" type="text" placeholder="Seu Sobrenome" value={this.state.lastname} onChange = {(e) => {this.handleChangeInput(e)}}/>
+          <input name="phone" type="tel" placeholder="Seu Telefone" value={this.state.phone} onChange = {(e) => {this.handleChangeInput(e)}} />
+          <input name="email" type="email" placeholder="Seu e-mail" value={this.state.email} onChange = {(e) => {this.handleChangeInput(e)}} />
+          <input name="password" type="password" placeholder="Senha Atual" value={this.state.password} onChange = {(e) => {this.handleChangeInput(e)}} />
+          <input name="newpassword" type="password" placeholder="Nova Senha" value={this.state.newpassword} onChange = {(e) => {this.handleChangeInput(e)}} />
+          <input name="confirmnewpassword" type="password" placeholder="Confirmar Nova Senha" value={this.state.confirmnewpassword} onChange = {(e) => {this.handleChangeInput(e)}} />
+
+          <button type="submit">Salvar Alterações</button>
+        </form>
+      </>
+    );
+  }
 }
