f7ac7c7846d22c1c43a84e8b74902526becb878a
heliobreve <habreve@gmail.com>
2019-08-14 21:13:34 -0300

    implementando UserProfile, modelo petfinder

--- frontend/src/pages/UserProfile/index.js
+++ frontend/src/pages/UserProfile/index.js
@@ -3,8 +3,21 @@
 import React from 'react';
 
 export default function UserProfile() {
-  return
-  <div> 
-  <h1>Home</h1>
-  </div>;
+  return (
+    <>
+      <h1>
+        <h2>User Profile</h2>
+      </h1>
+      <form>
+        <input type="text" placeholder="Seu Nome" />
+        <input type="text" placeholder="Seu Sobrenome" />
+        <input type="tel" placeholder="Seu Telefone" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua Senha" />
+
+        <button type="submit">Acessar</button>
+        
+      </form>
+    </>
+  );
 }
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -10,6 +10,7 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
 import PetProfile from '../pages/PetProfile';
+import UserProfile from '../pages/UserProfile';
 
 export default function Routes() {
   return (
@@ -18,6 +19,7 @@ export default function Routes() {
       <Route path="/signin" component={SignIn} />
       <Route path="/signup" component={SignUp} />
       <Route path="/petprofile" component={PetProfile} />
+      <Route path="/userprofile" component={UserProfile} />
     </Switch>
   );
 }
