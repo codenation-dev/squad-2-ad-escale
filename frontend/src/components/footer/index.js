baa10de6f3f14c42e6a832f3cbd1981876d56b37
Saulo Silva Castilhos <altfreedom@gmail.com>
2019-08-23 14:55:59 -0300

    fix: styles

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -6,7 +6,12 @@ export default function Footer() {
     <>
       <div style={{ bottom: '0', width: '100%' }}>
         <nav className="navbar sticky-bottom navbar-light bg-light">
-          <span className="navbar-brand mb-0 h1">Footer</span>
+          <span
+            className="navbar-brand mb-0 h1"
+            style={{ paddingLeft: '20px' }}
+          >
+            Footer
+          </span>
         </nav>
       </div>
     </>
