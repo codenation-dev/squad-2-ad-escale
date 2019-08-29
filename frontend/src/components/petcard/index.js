222672f827b22d11c5dcef5619ef111aa8b81923
Saulo Silva Castilhos <altfreedom@gmail.com>
2019-08-29 20:25:10 -0300

    fix: exibindo as imagens

--- frontend/src/components/petcard/index.js
+++ frontend/src/components/petcard/index.js
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 
 // Componente pet
 export default function Petcard({ pet }) {
-  const { id, image = {}, name, category, gender, city, state } = pet;
+  const { id, images = {}, name, category, city } = pet;
   const uri = `/petprofile/${id}`;
   return (
     <>
       <Link to={uri} style={{ clearfix: 'true' }}>
         <div className="col-sm w-100 p-3 mh-100">
           <div className="card">
-            {image[0] !== undefined ? (
+            {images[0] !== undefined ? (
               <img
                 className="card-img-top rounded mx-auto d-block"
                 style={{ padding: '1rem' }}
-                src={image[0]}
+                src={images[0].image}
                 alt="Card image cap"
               />
             ) : (
