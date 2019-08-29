bf84eedcdf7ca0bfbb5553797aed272b1c915d38
heliobreve <habreve@gmail.com>
2019-08-29 02:56:18 -0300

    Testando react e react-bootstrap

--- frontend/src/components/homemorefilter/index.js
+++ frontend/src/components/homemorefilter/index.js
@@ -20,6 +20,10 @@ export default function HomeMoreFilter({ handleGetFilteredPets }) {
         className="container"
         style={{ textAlign: 'center', padding: '12px' }}
       >
+
+        {/* Teste com <Link to = ""/> em droppdown button do bootstrap*/}
+
+
         <div className="btn-group" role="group" aria-label="Exemplo básico">
           <div className="btn-group">
             <button
@@ -49,6 +53,118 @@ export default function HomeMoreFilter({ handleGetFilteredPets }) {
           </div>
         </div>
       </div>
+
+      {/* Teste com <Link to = ""/> em droppdown button do bootstrap*/}
+
+
+      <div className="btn-group" role="group" aria-label="Exemplo básico">
+          <div className="btn-group">
+            <button
+              type="button"
+              className="btn btn-danger dropdown-toggle"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Tamanho
+            </button>
+            <div className="dropdown-menu">
+              <Link to="/" className="dropdown-item">
+                Mini
+              </Link>
+              <Link to="/" className="dropdown-item">
+                Pequeno
+              </Link>
+              <Link to="/" className="dropdown-item">
+                Medio
+              </Link>
+              <div className="dropdown-divider" />
+              <Link to="/SignIn" className="dropdown-item">
+                Grande
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+           
+      {/* Teste com Select do react bootstrap */}
+
+      <div
+        className="container"
+        style={{ textAlign: 'center', padding: '12px' }}
+      >
+        <div className="btn-group" role="group" aria-label="Exemplo básico">
+          <div className="btn-group">
+            <button
+              type="button"
+              className="btn btn-danger dropdown-toggle"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Tamanho
+            </button>
+            <div className="dropdown-menu">
+              <Link to="/" className="dropdown-item">
+                Mini
+              </Link>
+              <Link to="/" className="dropdown-item">
+                Pequeno
+              </Link>
+              <Link to="/" className="dropdown-item">
+                Medio
+              </Link>
+              <div className="dropdown-divider" />
+              <Link to="/SignIn" className="dropdown-item">
+                Grande
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
     </>
   );
 }
+
+
+
+
+
+/*
+<button
+            type="button"
+            className="btn btn-warning"
+            name="ANIMAL"
+            onClick={e => {
+              handleGetFilteredPets(e.target.name);
+            }}
+          >
+            Tamanho
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary dropdown-toogle"
+            name="TAMANHO"
+            onClick={e => {
+              handleGetFilteredPets(e.target.name);
+            }}
+          >
+            Tamanho
+          </button>
+          <button
+            type="button"
+            className="btn btn-info"
+            name="SEXO"
+            onClick={e => {
+              handleGetFilteredPets(e.target.name);
+            }}
+          >
+            Sexo
+          </button>
+
+*/
