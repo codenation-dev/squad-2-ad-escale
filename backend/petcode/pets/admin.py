0faa55e712ba137a51889d5cd8a0a8a755cc89d1
Karen Morais <katudda@hotmail.com>
2019-08-29 15:58:57 -0300

    backend refactoring

--- backend/petcode/pets/admin.py
+++ backend/petcode/pets/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-# from .models import Pet, PetType, Size, Gender, CategoryStatus, Category, CategoryAdmin, Image
 from .models import Pet, PetType, CategoryStatus, Category, CategoryAdmin, Image
 
 admin.site.register(Pet)
 admin.site.register(Image)
 admin.site.register(PetType)
-# admin.site.register(Size)
-# admin.site.register(Gender)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(CategoryStatus)
