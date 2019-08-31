080a0f0c7cf7234f77a55f247ad01b93ab70396f
Saulo Silva Castilhos <altfreedom@gmail.com>
2019-08-30 21:42:26 -0300

    fix: style form

--- frontend/src/components/addpet/index.js
+++ frontend/src/components/addpet/index.js
@@ -1,202 +1,158 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import axios from 'axios'
-import { Container, Form, Col, Button } from 'react-bootstrap';
-import { IoMdPaw } from 'react-icons/io';
-
-import { handleToken } from '../../containers/SignIn/actions';
+import axios from 'axios';
 
 class AddPet extends Component {
-  state = {
-    name: '',
-    category: '',
-    pet_type: '',
-    gender: '',
-    size: '',
-    contact_name: '',
-    email: '',
-    phone_1: '',
-    phone_2: '',
-    state: '',
-    city: '',
-    description: ''
+  state = {};
 
-  };
-
-  handleChangeUserId = value => {
-    this.setState({userId: value});
-  };
-
-  handleChangeNamePet = value => {
-    this.setState({name: value});
-  };
-
-  handleChangeCategory = value => {
-    this.setState({category: value});
-  };
-
-  handleChangeType = value => {
-    this.setState({pet_type: value});
-  };
-
-  handleChangeGender= value => {
-    this.setState({gender: value});
-  };
-
-  handleChangeSize= value => {
-    this.setState({size: value});
-  };
-
-  handleChangeContact= value => {
-    this.setState({contact_name: value});
-  };
-
-  handleChangeEmail= value => {
-    this.setState({email: value});
-  };
-
-  handleChangePhone1= value => {
-    this.setState({phone_1: value});
-  };
-
-  handleChangePhone2= value => {
-    this.setState({phone_2: value});
-  };
-
-  handleChangeCity= value => {
-    this.setState({city: value});
-  };
-
-  handleChangeState= value => {
-    this.setState({state: value});
-  };
-
-  handleChangeDescription= value => {
-    this.setState({description: value});
-  };
-
-  handleGetToken = value => {
-    this.props.dispatch(handleToken(value));
+  handleEachOneState = e => {
+    this.setState({ [e.name]: e.value });
   };
 
   handleAddPet = async e => {
     e.preventDefault();
-    const { userId, name, category, pet_type, gender, size, contact_name, email, phone_1, phone_2, state, city, description} = this.state;
+    const {
+      name,
+      city,
+      state,
+      description,
+      contact_name,
+      phone_1,
+      category,
+      pet_type,
+      size,
+      gender,
+    } = this.state;
 
-    var config = {
-      headers: { Authorization: `token ${this.props.token}` }
-      };
+    const config = {
+      headers: { Authorization: `token ${this.props.token}` },
+    };
 
     try {
-      console.log(this.state)
-        await axios
-          .post('https://petcode.pythonanywhere.com/api/pet/', {
-            userId, name, category, pet_type, gender, size, contact_name, email, phone_1, phone_2, state, city, description
+      console.log(this.state);
+      await axios
+        .post(
+          'https://petcode.pythonanywhere.com/api/pet/',
+          {
+            name,
+            city,
+            state,
+            description,
+            contact_name,
+            phone_1,
+            category,
+            pet_type,
+            size,
+            gender,
           },
-          config)
-          .then(response => {
-            const res = response.data;
-            console.log(res)
-          });
-      } catch (err) {
-        console.log(err)
-        this.setState({ error: 'Erro' });
-      }
+          config,
+        )
+        .then(response => {
+          const res = response.data;
+        });
+    } catch (e) {
+      alert(e);
     }
+  };
+
   render() {
-   return (
+    return (
       <>
-      <div className="row">
-          </div>
-          <div className="row">
-            <div className="img-fluid rounded mx-auto d-block">
-              <IoMdPaw className="img-fluid" size="135" />
+        <form>
+          <div className="form-row">
+            <div class="form-group col-md-4">
+              <input
+                type="text"
+                class="form-control"
+                id="name"
+                placeholder="Nome do pet"
+              />
+            </div>
+            <div className="form-group col-md-2">
+              <select id="category" class="form-control">
+                <option selected>Categoria</option>
+                <option value="">Achado</option>
+                <option value="">Perdido</option>
+                <option value="">Adoção</option>
+              </select>
+            </div>
+            <div className="form-group col col-md-2">
+              <select id="pet_type" class="form-control">
+                <option selected>Tipo</option>
+                <option value="">Gato</option>
+                <option value="">Cachorro</option>
+                <option value="">Outro</option>
+              </select>
+            </div>
+            <div className="form-group col col-md-2">
+              <select id="size" class="form-control">
+                <option selected>Porte</option>
+                <option value="">Pequeno</option>
+                <option value="">Médio</option>
+                <option value="">Grande</option>
+              </select>
+            </div>
+            <div className="form-group col col-md-2">
+              <select id="gender" class="form-control">
+                <option selected>Sexo</option>
+                <option value="">Fêmea</option>
+                <option value="">Macho</option>
+              </select>
             </div>
           </div>
-        <Form onSubmit={this.handleAddPet}>
-          <Container>
-            <Form.Row>
-              <Form.Group controlId="formGridUser">
-                <Form.Control placeholder="Id" onChange={e => this.handleChangeUserId(e.target.value)} />
-              </Form.Group>
+          <div className="form-row">
+            <div class="form-group col">
+              <textarea
+                class="form-control"
+                id="description"
+                rows="2"
+                placeholder="Detalhes do pet"
+              ></textarea>
+            </div>
+          </div>
 
-              <Form.Group controlId="formGridNamePet">
-                <Form.Control placeholder="Nome do Pet" onChange={e => this.handleChangeNamePet(e.target.value)}/>
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridCategoria" onChange={e => this.handleChangeCategory(e.target.value)}>
-                <Form.Control as="select">
-                  <option>Categoria...</option>
-                  <option>ADOCAO</option>
-                  <option>ENCONTRADOS</option>
-                  <option>PROCURA_SE</option>
-                </Form.Control>
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridTipo" onChange={e => this.handleChangeType(e.target.value)}>
-                <Form.Control as="select">
-                  <option>Tipo...</option>
-                  <option>1</option>
-                  <option>2</option>
-                </Form.Control>
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridGenero" onChange={e => this.handleChangeGender(e.target.value)}>
-                <Form.Control as="select">
-                  <option>Gênero...</option>
-                  <option>m</option>
-                  <option>f</option>
-                </Form.Control>
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridSize" onChange={e => this.handleChangeSize(e.target.value)}>
-                <Form.Control as="select">
-                  <option>Porte...</option>
-                  <option>p</option>
-                  <option>m</option>
-                  <option>g</option>
-                </Form.Control>
-              </Form.Group>
-            </Form.Row>
-
-            <Form.Group controlId="formGridDescription">
-                <Form.Control placeholder="Descrição" onChange={e => this.handleChangeDescription(e.target.value)}/>
-              </Form.Group>
-
-            <Form.Group as={Col} controlId="formGridContato">
-              <Form.Control type="text" placeholder="Nome do Contato" onChange={e => this.handleChangeContact(e.target.value)} />
-            </Form.Group>
-
-            <Form.Group as={Col} controlId="formGridEmail">
-              <Form.Control type="email" placeholder="Email do contato" onChange={e => this.handleChangeEmail(e.target.value)}/>
-            </Form.Group>
-
-            <Form.Group controlId="formGridPhone1">
-              <Form.Control placeholder="Celular" onChange={e => this.handleChangePhone1(e.target.value)}/>
-            </Form.Group>
-
-            <Form.Group controlId="formGridPhone2">
-              <Form.Control placeholder="Telefone" onChange={e => this.handleChangePhone2(e.target.value)}/>
-            </Form.Group>
-
-            <Form.Group as={Col} controlId="formGridState"onChange={e => this.handleChangeState(e.target.value)}>
-              <Form.Control as="select">
-                <option>UF...</option>
-                <option>SP</option>
-              </Form.Control>
-            </Form.Group>
-
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridCity">
-                <Form.Control placeholder="Cidade" onChange={e => this.handleChangeCity(e.target.value)}/>
-              </Form.Group>
-            </Form.Row>
-
-            <Button variant="primary" type="submit">
-              Adicionar
-            </Button>
-          </Container>
-        </Form>
+          <div className="form-row">
+            <div className="form-group col-md-4">
+              <input
+                type="text"
+                class="form-control"
+                id="contact_name"
+                placeholder="Seu nome"
+              />
+            </div>
+            <div className="form-group  col-md-2">
+              <input
+                type="text"
+                class="form-control"
+                id="phone_1"
+                placeholder="Seu telefone"
+              />
+            </div>
+            <div className="form-group col col-md-4">
+              <input
+                type="text"
+                class="form-control"
+                id="city"
+                placeholder="Cidade"
+              />
+            </div>
+            <div className="form-group col col-md-2">
+              <input
+                type="text"
+                class="form-control"
+                id="state"
+                placeholder="Estado"
+              />
+            </div>
+            {/* <div className="form-row">
+              <div class="form-group col-md-8">
+                <div className="button">
+                  <input type="file" id="multi" multiple />
+                </div>
+              </div>
+            </div> */}
+          </div>
+        </form>
       </>
     );
   }
